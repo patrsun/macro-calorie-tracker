ec6b1a6caa51c0e204372df39b1fc6d79d621e16
Patrick <patrsun14@gmail.com>
2024-06-11 18:34:10 -0400

    fix: enable CORS? 4

--- pages/api/daily.js
+++ pages/api/daily.js
@@ -69,6 +69,26 @@ import nextConnect from "next-connect";
 import middleware from "../../middleware/database";
 import { ObjectId } from "mongodb";
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 const handler = nextConnect();
 
 handler.use(middleware);
@@ -109,4 +129,4 @@ handler.post(async (req, res) => {
   res.json({ message: "ok" });
 });
 
-export default handler;
+module.exports = allowCors(handler);
